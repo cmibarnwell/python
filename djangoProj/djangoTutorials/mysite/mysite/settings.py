4ddbbe7677696d737436a87cbd9ba2de58d0f242
Caleb Barnwell <calebbarnwell@gmail.com>
2017-01-22 09:55:58 -0600

    Initial Commit

--- djangoProj/djangoTutorials/mysite/mysite/settings.py
+++ djangoProj/djangoTutorials/mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from settings_secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4wc-vc+v1*zh$k!b$5a+th)lifg2f(0%=1a=7#4hk7rtbbi@w4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ djangoProj/djangoTutorials/mysite/mysite/settings_secret.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '4wc-vc+v1*zh$k!b$5a+th)lifg2f(0%=1a=7#4hk7rtbbi@w4'
